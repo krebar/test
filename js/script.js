d12845ece0db59b6bdbeca846eec95c43880aea3
Alex <kreba.alex@gmail.com>
2020-12-14 22:29:07 +0100

    1

--- js/script.js
+++ js/script.js
@@ -1,75 +1,61 @@
 'use strict';
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    }
-}
-
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Один из последних просмотренных фильмов?", ""),
+                b = prompt("На сколько оцените его?", "");
+
+            if (a != null && a != "" && a.length < 50 && b != null && b != "" && b.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log("done");
+            } else {
+                console.log("Error");
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log("Вы классический зритель");
+
+        } else if (personalMovieDB.count > 30) {
+            console.log("Вы киноман");
+
+        } else {
+            console.log("Произошла ошибка");
+
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+
+            personalMovieDB.genres[i - 1] = prompt(`Сколько фильмов вы уже посмотрели ${i}`);
+        }
+    },
+
+    
 
 };
-function showMyDB(hidden){
-    if( !hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Один из последних просмотренных фильмов?", ""),
-            b = prompt("На сколько оцените его?", "");
-
-        if (a != null && a != "" && a.length < 50 && b != null && b != "" && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("done");
-        } else {
-            console.log("Error");
-            i--;
-        }
-    }
-}
-rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log("Вы классический зритель");
-
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-
-    } else {
-        console.log("Произошла ошибка");
-
-    }
-}
- detectPersonalLevel();
-
-
-
-
- function writeYourGenres(){
-    for (let i = 1; i <= 3; i++) {
-   
-      personalMovieDB.genres[i - 1] = prompt(`Сколько фильмов вы уже посмотрели ${i}`);
-    }
-}
-writeYourGenres();
--- js/test.js
+++ js/test.js
@@ -1,4 +1,29 @@
-function showMyDB(){
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log(Object.keys(options).length);
+    }
+};
 
-    
+options.makeTest();
+const {} = options.colors;
+console.log(border);
+
+for (let key in options){
+    if (typeof(options[key]) === "object") {
+        for(let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            
+        }
+     } else {
+            console.log(`Свойство ${key} имеет значение ${options[key]}`);
+            
+     }
+
 }
